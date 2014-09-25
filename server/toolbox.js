118e69b09952889b06a8abfab47731b924d62890
shaialon <shai@totango.com>
2014-09-26 00:34:16 +0300

    Calling products

--- server/toolbox.js
+++ server/toolbox.js
@@ -37,5 +37,36 @@ Meteor.methods({
       , {multi: true}
       );
     }
+  },
+  getProducts:function(aa){
+    return HTTP.call("POST", "https://www.popshops.com/v3/products.json",
+           {params: {
+                      account: 'bbhntrjt16yvunll9iyayufn4',
+                      catalog: '2p3tubvtj2b3t7dmvo5k0vdn5',
+                      results_per_page: 100,
+                      category:'jewlery',
+                    }}
+                    );
+    // return $.ajax({
+    //     url: 'https://www.popshops.com/v3/products.json?account=bbhntrjt16yvunll9iyayufn4&catalog=2p3tubvtj2b3t7dmvo5k0vdn5&results_per_page=100&category=jewlery',
+    //     //https://www.popshops.com/v3/products.json?account=bbhntrjt16yvunll9iyayufn4&catalog=db46yl7pq0tgy9iumgj88bfj7&results_per_page=100
+
+    //     // data: formData,
+    //     // dataType: "jsonp",
+    //     // jsonpCallback: "localJsonpCallback",
+    //     // type: "POST",
+
+    //     type: "POST",
+    //       // crossDomain: true,
+    //       // data: data,
+    //       dataType: "json",
+    //       success:function(result){
+    //           return (JSON.stringify(result));
+    //       },
+    //       error:function(xhr,status,error){
+    //           return (status);
+    //       }
+
+    // });
   }
 });
